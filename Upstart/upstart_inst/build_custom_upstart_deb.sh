9dc5a3038c83d64f8e8bd39789b1d8780719bd89
Kashif Ali <kashif.a@plumgrid.com>
2016-10-20 15:55:57 +0500

    Pushing license copies to both Quagga and Upstart - Adding copyright notice to upstart bash script. - Patchset2: changed license file from LGPL to GPLv2
    Signed-off-by: Kashif Ali <kashif.a@plumgrid.com>

--- Upstart/upstart_inst/build_custom_upstart_deb.sh
+++ Upstart/upstart_inst/build_custom_upstart_deb.sh
@@ -1,3 +1,22 @@
+# This file is part of upstart_1.5-0ubuntu7.4_amd64, a modified version of 
+# upstart_1.5 used by PLUMgrid.
+
+# Copyright (c) 2016, PLUMgrid Inc, http://plumgrid.com
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 #!/bin/bash
 
 set -e
@@ -18,7 +37,6 @@ zcat "${SRCDIR}/upstart_1.5-0ubuntu7.3.diff.gz" | patch -p0
 
 # apply and document our patch(es)
 pushd "${WORKDIR}"
-export DEBEMAIL="PLUMgrid package bot <eng@plumgrid.com>"
 patch -p1 < "${SRCDIR}/upstart_1.5-dbus_address.patch"
 debchange -i 'Add domain socket suffix for running inside shared network lxc container'
 
